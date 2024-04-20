2d123ddfe3f90aee790d41cb179e843048b38dbd
bouzenaali <Alibouzena16@gmail.com>
2024-04-20 20:14:14 +0100

    fix: fix the inference engine

--- field_suggestion.py
+++ field_suggestion.py
@@ -1,20 +1,32 @@
 from aima3.logic import *
-from knowledge_base import kb
+from knowledge_base import kb, fields
 
 def suggest_fields(preferences):
+    """
+    Suggest fields of study based on the given user preferences.
+    
+    Args:
+        preferences (dict): A dictionary containing the user's preferences.
+            The keys should be the question names (e.g., "PreferredField",
+            "PreferredJobProspects", "PreferredHighSchool", "PreferredType")
+            and the values should be the selected options.
+    
+    Returns:
+        list: A list of recommended fields of study.
+    """
+    recommended_fields = []
 
-    # Add user preferences to the knowledge base
-    for preference, value in preferences.items():
-        if value != "None":
-            kb.tell(expr(f'{preference}(Me, "{value}")'))
+    # Add the user's preferences to the knowledge base
+    for question, option in preferences.items():
+        kb.tell(expr(f'{question}(Me, "{option}")'))
 
-    # Print out the contents of the KB
+    # Query the knowledge base for recommended fields
+    for field in fields:
+        if kb.ask(expr(f'RecommendField("{field["name"]}", Me)')):
+            recommended_fields.append(field["name"])
 
-    recommendations = []
+    # Remove the user's preferences from the knowledge base
+    for question, option in preferences.items():
+        kb.retract(expr(f'{question}(Me, "{option}")'))
 
-    # Retrieve recommended fields based on preferences and constraints
-    for field in fol_fc_ask(kb, expr('RecommendField(x, Me)')):
-        recommendations.append(field.bindings[0][x])
-        print("Recommendation:", field.bindings[0][x])
-
-    return recommendations
+    return recommended_fields
